9165bca40d2908b60fceb4a67f37bc0d9d6b89c5
Trevis42 <sirtrevis42@gmail.com>
2020-07-23 11:14:46 -0500

    fixed package issue

--- api/src/main/java/com/talentpath/amazin/api/customerWhoBoughtAlso/CustomerWhoBoughtAlso.java
+++ api/src/main/java/com/talentpath/amazin/api/customerWhoBoughtAlso/CustomerWhoBoughtAlso.java
@@ -1,15 +1,16 @@
 package com.talentpath.amazin.api.customerWhoBoughtAlso;
+
 import java.util.ArrayList;
 
 public class CustomerWhoBoughtAlso {
     private Long id;
     private String otherIds;
 
-    public CustomerWhoBoughtAlso(){
+    public CustomerWhoBoughtAlso() {
 
     }
 
-    public CustomerWhoBoughtAlso(long id, String otherIds){
+    public CustomerWhoBoughtAlso(long id, String otherIds) {
         super();
         this.id = id;
         this.otherIds = otherIds;
@@ -27,7 +28,7 @@ public class CustomerWhoBoughtAlso {
         return otherIds;
     }
 
-    public void setOtherIds( String otherIds) {
+    public void setOtherIds(String otherIds) {
         this.otherIds = otherIds;
     }
 
@@ -42,23 +43,26 @@ public class CustomerWhoBoughtAlso {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
         CustomerWhoBoughtAlso other = (CustomerWhoBoughtAlso) obj;
         // id comparison
         if (id == null) {
-            if (other.id != null) return false;
-        }
-        else if (!id.equals(other.id)) return false;
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
         // arrayOfOtherIds comparison
         if (otherIds == null) {
-            if (other.otherIds != null) return false;
-        }
-        else if (!otherIds.equals(other.otherIds)) return false;
+            if (other.otherIds != null)
+                return false;
+        } else if (!otherIds.equals(other.otherIds))
+            return false;
         return true;
     }
-
-
 
 }
