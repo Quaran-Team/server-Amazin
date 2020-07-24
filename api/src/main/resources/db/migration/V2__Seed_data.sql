746f85f337eb7a3488a50ba69a4e0e99526cd2e4
Trevis42 <sirtrevis42@gmail.com>
2020-07-24 09:10:44 -0500

    added seed data to v2 migration

---
+++ api/src/main/resources/db/migration/V2__Seed_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO Items (
+    itemName,
+    itemPrice,
+    itemTitle,
+    itemAbout,
+    itemDescription
+)
+VALUES (
+    'Totally Awesome product',
+    1000.00,
+    'TotallY awesome neat, fastest ever, non-GMO, product version 12',
+    '{"this is a great product","it looks great"}',
+    'This thing is a product, it works, etc. etc...'
+)
+
+INSERT INTO Items (
+        itemName,
+        itemPrice,
+        itemTitle,
+        itemAbout,
+        itemDescription
+    )
+VALUES (
+        'Tech virtual drone online browser',
+        5432.12,
+        'Tech virtual drone online browser platform through in a system',
+        '{"Now digital designs id anywhere atoms.","Now strategy startups documents designs.","Venture crypto adopters niche.","Video algorithm system ultra-private policies engineering.","Users takedowns."}',
+        'Tech virtual drone online browser platform through in a system.But stream software offline.Professor install angel sector anywhere create at components smart.Document fab developers encryption smartphone powered, bespoke blockstack edit atoms.Companies a storage adopters.Hardware company planet, torrent ut developers stream, engineering keyphrase end.'
+    )
