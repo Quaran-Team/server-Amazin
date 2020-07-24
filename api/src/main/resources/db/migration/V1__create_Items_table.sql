1095e8f758d6b8bdbf36159077be9bc60feb345a
Trevis42 <sirtrevis42@gmail.com>
2020-07-24 13:17:31 -0500

    updating creation of db

--- api/src/main/resources/db/migration/V1__create_Items_table.sql
+++ api/src/main/resources/db/migration/V1__create_Items_table.sql
@@ -1,14 +1,14 @@
 -- Table: public.items
 -- DROP TABLE public.items;
 CREATE TABLE public.items (
-    itemId bigint NOT NULL GENERATED ALWAYS AS IDENTITY (
+    itemId bigint GENERATED ALWAYS AS IDENTITY (
         INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1
     ),
-    itemName character varying COLLATE pg_catalog."default" NOT NULL,
-    itemPrice numeric(10, 2) NOT NULL DEFAULT 1.0,
-    itemTitle text COLLATE pg_catalog."default" NOT NULL DEFAULT 'long(er) product title/name'::text,
-    itemAbout text [] COLLATE pg_catalog."default" NOT NULL DEFAULT 'This is a great product. It looks great'::text,
-    itemDescription text COLLATE pg_catalog."default" NOT NULL DEFAULT 'This is a product, etc., etc.'::text
+    itemName text NOT NULL,
+    itemPrice numeric(10, 2) NOT NULL DEFAULT '1.00',
+    itemTitle text NOT NULL DEFAULT 'long(er) product title/name'::text,
+    itemAbout text NOT NULL DEFAULT 'This is a great product. It looks great'::text,
+    itemDescription text NOT NULL DEFAULT 'This is a product, etc., etc.'::text
 ) TABLESPACE pg_default;
 ALTER TABLE public.items OWNER to "postgres";
 COMMENT ON TABLE public.items IS 'Items table. This should hold most of the item data needed for the site.';
