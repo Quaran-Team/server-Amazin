aeff152b55f872f88148abe6817e8ce72cf7a754
JC <avalojc@gmail.com>
2020-07-30 08:47:08 -0400

    created communication between db and routes

--- api/src/main/java/com/talentpath/amazin/api/customerImage/CustomerImageResource.java
+++ api/src/main/java/com/talentpath/amazin/api/customerImage/CustomerImageResource.java
@@ -3,27 +3,35 @@ package com.talentpath.amazin.api.customerImage;
 import java.net.URI;
 import java.util.List;
 
+import com.talentpath.amazin.api.customerImage.CustomerImageRepo;
+
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = { "http://localhost:3000" })
+@RequestMapping("/customerImages")
 @RestController
 public class CustomerImageResource {
 
     @Autowired
-    private CustomerImagesHardcodedService customerImageManagementService;
+    private CustomerImageRepo customerImageRepo;
 
-    @GetMapping("/customerImages")
+    @GetMapping("/")
+    @Query("SELECT * FROM jca0")
     public List<CustomerImage> getAllCustomerImages() {
-        return customerImageManagementService.findAll();
+        return (List<CustomerImage>) customerImageRepo.findAll();
     }
 
-    @GetMapping("/customerImages/{id}")
-    public CustomerImage getCustomerImage( @PathVariable long id) {
-        return customerImageManagementService.findById(id);
-    }
+//    @GetMapping("/{id}")
+//    @Query("SELECT * FROM jca0")
+//    public List<CustomerImage> getCustomerImagesById(@PathVariable("id") long id) {
+//        return (List<CustomerImage>) customerImageRepo.getOne(id);
+//    }
 }
