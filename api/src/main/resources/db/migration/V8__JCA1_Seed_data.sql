1cf7a3144a31425330b80d84b5892afbe13c69c3
JC <avalojc@gmail.com>
2020-08-03 11:52:37 -0400

    corrected column name on data seed b8. created routes to reach db for customer who bought also class

--- api/src/main/resources/db/migration/V8__JCA1_Seed_data.sql
+++ api/src/main/resources/db/migration/V8__JCA1_Seed_data.sql
@@ -1,119 +1,119 @@
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '6,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,7,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,8,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,9,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,10'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
     );
 INSERT INTO public.jca1 (
-        otherItem
+        otherItems
     )
 VALUES (
         '1,2,3,4,5'
