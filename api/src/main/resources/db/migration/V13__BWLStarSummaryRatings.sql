d2acfdac68aa50c10891f39abcc6887ef66e6348
BLoveless <bloveless@talentpath.com>
2020-08-04 17:53:14 -0400

    it works

--- api/src/main/resources/db/migration/V13__BWLStarSummaryRatings.sql
+++ api/src/main/resources/db/migration/V13__BWLStarSummaryRatings.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE public.bwl01
 (
-    ratingid bigint GENERATED ALWAYS AS IDENTITY,
+    ratingid bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     overallrating numeric(10, 1),
     totalfivestarrating integer,
     totalfourstarrating integer,
@@ -22,16 +22,16 @@ COMMENT ON TABLE public.bwl01
 IS 'temporary';
 
 INSERT INTO public.bwl01 (overallRating, totalFiveStarRating, totalFourStarRating, totalThreeStarRating, totalTwoStarRating,totalOneStarRating)
-VALUES ('4.8',"40","30","20","10","5");
+VALUES (4.8,40,30,20,10,5);
 
 INSERT INTO public.bwl01 (overallRating, totalFiveStarRating, totalFourStarRating, totalThreeStarRating, totalTwoStarRating,totalOneStarRating)
-VALUES ('4.4',"41","33","10","10","5");
+VALUES (4.4,41,33,10,10,5);
 
 INSERT INTO public.bwl01 (overallRating, totalFiveStarRating, totalFourStarRating, totalThreeStarRating, totalTwoStarRating,totalOneStarRating)
-VALUES ('3.8',"20","38","17","11","3");
+VALUES (3.8,20,38,17,11,3);
 
 INSERT INTO public.bwl01 (overallRating, totalFiveStarRating, totalFourStarRating, totalThreeStarRating, totalTwoStarRating,totalOneStarRating)
-VALUES ('4.1',"41","31","21","11","1");
+VALUES (4.1,41,31,21,11,1);
 
 INSERT INTO public.bwl01 (overallRating, totalFiveStarRating, totalFourStarRating, totalThreeStarRating, totalTwoStarRating,totalOneStarRating)
-VALUES ('2.8',"4","3","2","100","5");
+VALUES (2.8,4,3,2,100,5);
