935d325641e7ad061e0a3c31fb16d848a4c1b74f
BLoveless <bloveless@talentpath.com>
2020-08-07 14:08:58 -0400

    mergin

--- api/src/test/java/com/talentpath/amazin/api/BWLStarRouteTest.java
+++ api/src/test/java/com/talentpath/amazin/api/BWLStarRouteTest.java
@@ -1,17 +1,11 @@
 package com.talentpath.amazin.api;
 
 import io.restassured.RestAssured;
-
-import org.testng.Assert;
-
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.hasItems;
 
@@ -21,7 +15,7 @@ public class BWLStarRouteTest {
      //first test using api from tutorial - some weird error they don't work yet
     @Test
     public void test_GET_bwl01() {
-        Response response = RestAssured.get("https://reqres.in/api/");
+        Response response = RestAssured.get("//reqres.in/api/");
 
         System.out.println(response.asString());
         System.out.println(response.getBody().asString());
@@ -33,18 +27,6 @@ public class BWLStarRouteTest {
         int statusCode = response.getStatusCode();
 
         Assert.assertEquals(statusCode, 200);
-//        Response response = RestAssured.get("https://reqres.in/api/");
-//
-//        System.out.println(response.asString());
-//        System.out.println(response.getBody().asString());
-//        System.out.println(response.getStatusCode());
-//        System.out.println(response.getStatusLine());
-//        System.out.println(response.getHeader("content-type"));
-//        System.out.println(response.getTime());
-//
-//        int statusCode = response.getStatusCode();
-//
-//        Assert.assertEquals(statusCode, 200);
 
     }
 
@@ -52,23 +34,23 @@ public class BWLStarRouteTest {
     public void
     our_api_works() {
 
-//        when().
-//                get("/api/v1/totalstars").
-//                then().
-//                statusCode(200).
-//                body("ratingId", hasItems(1, 2));
+        when().
+                get("/api/v1/totalstars").
+                then().
+                statusCode(200).
+                body("ratingId", hasItems(1, 2));
 
 
     }
 
 
-    @Test
-    public void test_1 () {
-
+//    @Test
+//    public void test_1 () {
+//
 //        given().
 //                get(" https://reqres.in/api/users?page=2").then()
 //                .statusCode(200);
-    }
+//    }
 
 
 
