6179ad473261b589fb02eefca51984eb84a0c869
BLoveless <bloveless@talentpath.com>
2020-08-10 16:54:03 -0400

    cors error fixing

--- api/src/main/java/com/talentpath/amazin/api/summaryratingstars/SummaryStarRatingsController.java
+++ api/src/main/java/com/talentpath/amazin/api/summaryratingstars/SummaryStarRatingsController.java
@@ -1,17 +1,15 @@
 package com.talentpath.amazin.api.summaryratingstars;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.*;
 
 
+
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1")
 
