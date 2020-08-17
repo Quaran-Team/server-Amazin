a3efa5839b7f5f55602c42983bf058e469f3dbd7
Bilikis Orulebaja <borulebaja@talentpath.com>
2020-08-17 11:37:19 -0500

    added new columns & seeded data

--- api/src/main/resources/db/migration/V12__BOCRSeed_data.sql
+++ api/src/main/resources/db/migration/V12__BOCRSeed_data.sql
@@ -9,18 +9,43 @@ INSERT INTO public.customerreviews (
     )
 VALUES (
         1,
-       'bilikis',
-       'Great buy',
-       'reviewtag1, reviewtag2, reviewtag3, reviewtag4',
+       'Sheba O.',
+       'Too expensive',
+       'Vokalia, Consonantia, mountains, texts',
        'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
         there live the blind texts.
         Separated they live in Bookmarksgrove right at the coast of the Semantics,
         a large language ocean.
         A small river named Duden flows by their place and supplies it with the necessary regelialia.
         It is a paradisematic country, in which roasted parts',
-        4.5,
+        1.0,
         'Reviewed in the United States on August 13, 2020'
     );
+
+INSERT INTO public.customerreviews (
+        itemId,
+        reviewer,
+        reviewTitle,
+        reviewTag,
+        reviewBody,
+        rating,
+        date
+    )
+VALUES (
+        1,
+       'Bilikis O.',
+       'Product looks cheap',
+       'Semantics, coast, texts, language',
+       'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+        there live the blind texts.
+        Separated they live in Bookmarksgrove right at the coast of the Semantics,
+        a large language ocean.
+        A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        It is a paradisematic country, in which roasted parts',
+        1.0,
+        'Reviewed in the United States on August 11, 2020'
+    );
+
 INSERT INTO public.customerreviews (
         itemId,
         reviewer,
@@ -32,10 +57,29 @@ INSERT INTO public.customerreviews (
     )
 VALUES (
         2,
-       'bebe',
+       'Queen Bebe',
        'Damaged',
-       'reviewtag1, reviewtag2, reviewtag3, reviewtag4',
+       'totam, omnis, aperiam, eaque',
        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab',
         2.0,
         'Reviewed in the United States on August 10, 2020'
     );
+
+INSERT INTO public.customerreviews (
+        itemId,
+        reviewer,
+        reviewTitle,
+        reviewTag,
+        reviewBody,
+        rating,
+        date
+    )
+VALUES (
+        3,
+       'Nicki Minaj',
+       'Yikes!',
+       'unde, error, laudantium, totam',
+       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab',
+        2.0,
+        'Reviewed in the United States on August 12, 2020'
+ );
