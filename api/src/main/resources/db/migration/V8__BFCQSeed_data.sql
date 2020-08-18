0f3f39133bee1bc7b4a19fd20ac13ace3243886a
Brandt Campbell <BCampbell@talentpath.com>
2020-08-18 13:20:43 -0400

    final seeding

--- api/src/main/resources/db/migration/V8__BFCQSeed_data.sql
+++ api/src/main/resources/db/migration/V8__BFCQSeed_data.sql
@@ -58,3 +58,23 @@ VALUES (
         'Is it waterproof?',
         '3'
     );
+INSERT INTO public.questions (question, itemId)
+VALUES (
+        'Since you have to stand there to put popcorn or whatever in the microwave, why does it need to be voice controlled?',
+        '3'
+    );
+INSERT INTO public.questions (question, itemId)
+VALUES (
+        'Does this come with the standard flat blade, whisk, & cover like the new ones do?',
+        '13'
+    );
+INSERT INTO public.questions (question, itemId)
+VALUES (
+        'Will it work with amazon 4K fire stick?',
+        '3'
+    );
+INSERT INTO public.questions (question, itemId)
+VALUES (
+        'Does it stop my 6 year old from randomly running it w Alexa?',
+        '3'
+    );
